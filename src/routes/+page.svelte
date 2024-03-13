f0c0605fd4b35a031036908036cc3eefd3247bb0
Selfsigned <me@selfsigned.dev>
2024-03-13 15:47:34 +0100

    Switch to static adapter and deploy to github pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import Resume from '$lib/resume/Resume.svelte';
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 
-	const forPrint = !!$page.url.searchParams.get('print');
-	const theme = $page.url.searchParams.get('theme');
+	let forPrint: boolean;
+	let theme: string | null;
+
+	onMount(() => {
+		forPrint = !!$page.url.searchParams.get('print');
+		theme = $page.url.searchParams.get('theme');
+	});
 </script>
 
 <Resume {forPrint} data-theme={theme} />
